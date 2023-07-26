bdc89e10eb30bb158a631a1854b15af96fcaf8ae
zakiabuqaoud <20172106@student.alazhar.edu.ps>
2023-07-26 01:53:20 -0800

    final 2

--- lib/screens/drop_down_screen.dart
+++ lib/screens/drop_down_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:know_your_weather/constants.dart';
 
 import '../services/weather.dart';
 import '../widgets/drop_down_search.dart';
 import 'location_screen.dart';
+
 class DropDownScreen extends StatefulWidget {
   const DropDownScreen({Key? key}) : super(key: key);
 
@@ -13,7 +15,7 @@ class DropDownScreen extends StatefulWidget {
 class _DropDownScreenState extends State<DropDownScreen> {
   String _selectedOption = '';
 
-  getCityWeather(city)async{
+  getCityWeather(city) async {
     WeatherModel weatherInfo = WeatherModel();
     await weatherInfo.getCityLocationWeather(city);
     if (mounted) {
@@ -24,15 +26,18 @@ class _DropDownScreenState extends State<DropDownScreen> {
       }));
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
           child: Form(
             child: Column(
               children: [
                 SearchableDropdownFormField(
+
                   labelText: 'Select an city',
                   options: const ['paris', 'gaza', 'tokyo', "mumbai", "dubai"],
                   onChanged: (value) {
@@ -41,14 +46,30 @@ class _DropDownScreenState extends State<DropDownScreen> {
                     });
                   },
                 ),
+                const SizedBox(height: 8.0),
                 Text('Selected option: $_selectedOption'),
-                ElevatedButton(onPressed: ()async{ await getCityWeather(_selectedOption);}, child: const Text("get Weather"))
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () async {
+                    await getCityWeather(_selectedOption);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: conPrimaryColor,
+                  ),
+                  child: const Text(
+                    "get Weather",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
-
   }
 }
